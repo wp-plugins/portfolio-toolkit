61930c7a81715e2341f0c26ed356b7760e22878e
iamdmitrymayorov <iamdmitrymayorov@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-13 18:58:17 +0000

    Initial release.
    git-svn-id: https://plugins.svn.wordpress.org/portfolio-toolkit/trunk@1180338 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/class-portfolio-toolkit-deactivator.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://dmitrymayorov.com/
+ * @since      0.1.0
+ *
+ * @package    Portfolio_Toolkit
+ * @subpackage Portfolio_Toolkit/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * @since      0.1.0
+ * @package    Portfolio_Toolkit
+ * @subpackage Portfolio_Toolkit/includes
+ * @author     Dmitry Mayorov <iamdmitrymayorov@gmail.com>
+ */
+class Portfolio_Toolkit_Deactivator {
+
+	/**
+	 * Flush rewrite rules when plugin is deactivated. 
+	 *
+	 * @since    0.1.0
+	 */
+	public static function deactivate() {
+		flush_rewrite_rules();
+	}
+
+}
